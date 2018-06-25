2db3f9bc14015676fac8b495c270d99668da3db8
Gabriel Malet <gabrielmalet@gmail.com>
2018-06-25 22:49:52 +0200

    Fix some stuff

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 ## Dockerized spf13-vim
 
-### Install
+### Install Local
+
+`bash ./install.sh && source ~/.bash_docker.sh`
+
+### Install Remote
 
 `bash <(curl -sL https://raw.githubusercontent.com/UbikZ/dockerz/master/install.sh) && source ~/.bash_docker.sh`
 
--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ readonly bashDockerFile=.bash_docker.sh
 [[ -z $(cat ${bashRc} | grep "${bashDockerFile}") ]] && (echo "[ -f ~/${bashDockerFile} ] && source ~/${bashDockerFile}" >> "${bashRc}")
 
 cat >~/${bashDockerFile} <<EOL
-    alias uvi='docker run -ti -v $(pwd):/home/workspace/vim --rm ubikz/vim'
-    alias unode='docker run -ti -v $(pwd):/home/workspace/node -v ~/.npmrc:/root/.npmrc:ro --rm ubikz/node --rm'
-    alias ujava='docker run -ti -v $(pwd):/home/workspace/java -v ~/.m2:/root/.m2:ro --rm ubikz/java --rme'
+    alias uvi='docker run -ti -v \$(pwd):/home/workspace/vim --rm ubikz/vim'
+    alias unode='docker run -ti -v \$(pwd):/home/workspace/node -v ~/.npmrc:/root/.npmrc:ro --rm ubikz/node'
+    alias ujava='docker run -ti -v \$(pwd):/home/workspace/java -v ~/.m2:/root/.m2 --rm ubikz/java'
 EOL
 
 source ~/${bashDockerFile}
